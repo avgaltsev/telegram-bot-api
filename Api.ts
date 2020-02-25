3f90aadd2fadee52f51ae690dc5b9539c7497cc3
Anton Galtsev <avgaltsev@gmail.com>
2020-02-25 00:46:24 -0800

    Readonly removed

--- Api.ts
+++ Api.ts
@@ -1,268 +1,268 @@
 import Core from "./Core";
 
 export interface Update {
-	readonly update_id: number;
-	readonly message?: Message;
-	readonly edited_message?: Message;
-	readonly channel_post?: Message;
-	readonly edited_channel_post?: Message;
-	readonly inline_query?: InlineQuery;
-	readonly chosen_inline_result?: ChosenInlineResult;
-	readonly callback_query?: CallbackQuery;
-	readonly shipping_query?: ShippingQuery;
-	readonly pre_checkout_query?: PreCheckoutQuery;
+	update_id: number;
+	message?: Message;
+	edited_message?: Message;
+	channel_post?: Message;
+	edited_channel_post?: Message;
+	inline_query?: InlineQuery;
+	chosen_inline_result?: ChosenInlineResult;
+	callback_query?: CallbackQuery;
+	shipping_query?: ShippingQuery;
+	pre_checkout_query?: PreCheckoutQuery;
 }
 
 export interface WebhookInfo {
-	readonly url: string;
-	readonly has_custom_certificate: boolean;
-	readonly pending_update_count: number;
-	readonly last_error_date?: number;
-	readonly last_error_message?: string;
-	readonly max_connections?: number;
-	readonly allowed_updates?: string[];
+	url: string;
+	has_custom_certificate: boolean;
+	pending_update_count: number;
+	last_error_date?: number;
+	last_error_message?: string;
+	max_connections?: number;
+	allowed_updates?: string[];
 }
 
 export interface User {
-	readonly id: number;
-	readonly first_name: string;
-	readonly last_name?: string;
-	readonly username?: string;
-	readonly language_code?: string;
+	id: number;
+	first_name: string;
+	last_name?: string;
+	username?: string;
+	language_code?: string;
 }
 
 export interface Chat {
-	readonly id: number;
-	readonly type: "private" | "group" | "supergroup" | "channel";
-	readonly title?: string;
-	readonly username?: string;
-	readonly first_name?: string;
-	readonly last_name?: string;
-	readonly all_members_are_administrators?: boolean;
-	readonly photo?: ChatPhoto;
-	readonly description?: string;
-	readonly invite_link?: string;
+	id: number;
+	type: "private" | "group" | "supergroup" | "channel";
+	title?: string;
+	username?: string;
+	first_name?: string;
+	last_name?: string;
+	all_members_are_administrators?: boolean;
+	photo?: ChatPhoto;
+	description?: string;
+	invite_link?: string;
 }
 
 export interface Message {
-	readonly message_id: number;
-	readonly from?: User;
-	readonly date: number;
-	readonly chat: Chat;
-	readonly forward_from?: User;
-	readonly forward_from_chat?: Chat;
-	readonly forward_from_message_id?: number;
-	readonly forward_date?: number;
-	readonly reply_to_message?: Message;
-	readonly edit_date?: number;
-	readonly text?: string;
-	readonly entities?: MessageEntity[];
-	readonly audio?: Audio;
-	readonly document?: Document;
-	readonly game?: Game;
-	readonly photo?: PhotoSize[];
-	readonly sticker?: Sticker;
-	readonly video?: Video;
-	readonly voice?: Voice;
-	readonly video_note?: VideoNote;
-	readonly new_chat_members?: User[];
-	readonly caption?: string;
-	readonly contact?: Contact;
-	readonly location?: Location;
-	readonly venue?: Venue;
-	readonly new_chat_member?: User;
-	readonly left_chat_member?: User;
-	readonly new_chat_title?: string;
-	readonly new_chat_photo?: PhotoSize[];
-	readonly delete_chat_photo?: boolean;
-	readonly group_chat_created?: boolean;
-	readonly supergroup_chat_created?: boolean;
-	readonly channel_chat_created?: boolean;
-	readonly migrate_to_chat_id?: number;
-	readonly migrate_from_chat_id?: number;
-	readonly pinned_message?: Message;
-	readonly invoice?: Invoice;
-	readonly successful_payment?: SuccessfulPayment;
+	message_id: number;
+	from?: User;
+	date: number;
+	chat: Chat;
+	forward_from?: User;
+	forward_from_chat?: Chat;
+	forward_from_message_id?: number;
+	forward_date?: number;
+	reply_to_message?: Message;
+	edit_date?: number;
+	text?: string;
+	entities?: MessageEntity[];
+	audio?: Audio;
+	document?: Document;
+	game?: Game;
+	photo?: PhotoSize[];
+	sticker?: Sticker;
+	video?: Video;
+	voice?: Voice;
+	video_note?: VideoNote;
+	new_chat_members?: User[];
+	caption?: string;
+	contact?: Contact;
+	location?: Location;
+	venue?: Venue;
+	new_chat_member?: User;
+	left_chat_member?: User;
+	new_chat_title?: string;
+	new_chat_photo?: PhotoSize[];
+	delete_chat_photo?: boolean;
+	group_chat_created?: boolean;
+	supergroup_chat_created?: boolean;
+	channel_chat_created?: boolean;
+	migrate_to_chat_id?: number;
+	migrate_from_chat_id?: number;
+	pinned_message?: Message;
+	invoice?: Invoice;
+	successful_payment?: SuccessfulPayment;
 }
 
 export interface MessageEntity {
-	readonly type: "mention" | "hashtag" | "bot_command" | "url" | "email" | "bold" | "italic" | "code" | "pre" | "text_link" | "text_mention";
-	readonly offset: number;
-	readonly length: number;
-	readonly url?: string;
-	readonly user?: User;
+	type: "mention" | "hashtag" | "bot_command" | "url" | "email" | "bold" | "italic" | "code" | "pre" | "text_link" | "text_mention";
+	offset: number;
+	length: number;
+	url?: string;
+	user?: User;
 }
 
 export interface PhotoSize {
-	readonly file_id: string;
-	readonly width: number;
-	readonly height: number;
-	readonly file_size?: number;
+	file_id: string;
+	width: number;
+	height: number;
+	file_size?: number;
 }
 
 export interface Audio {
-	readonly file_id: string;
-	readonly duration: number;
-	readonly performer?: string;
-	readonly title?: string;
-	readonly mime_type?: string;
-	readonly file_size?: number;
+	file_id: string;
+	duration: number;
+	performer?: string;
+	title?: string;
+	mime_type?: string;
+	file_size?: number;
 }
 
 export interface Document {
-	readonly file_id: string;
-	readonly thumb?: PhotoSize;
-	readonly file_name?: string;
-	readonly mime_type?: string;
-	readonly file_size?: number;
+	file_id: string;
+	thumb?: PhotoSize;
+	file_name?: string;
+	mime_type?: string;
+	file_size?: number;
 }
 
 export interface Sticker {
-	readonly file_id: string;
-	readonly width: number;
-	readonly height: number;
-	readonly thumb?: PhotoSize;
-	readonly emoji?: string;
-	readonly file_size?: number;
+	file_id: string;
+	width: number;
+	height: number;
+	thumb?: PhotoSize;
+	emoji?: string;
+	file_size?: number;
 }
 
 export interface Video {
-	readonly file_id: string;
-	readonly width: number;
-	readonly height: number;
-	readonly duration: number;
-	readonly thumb?: PhotoSize;
-	readonly mime_type?: string;
-	readonly file_size?: number;
+	file_id: string;
+	width: number;
+	height: number;
+	duration: number;
+	thumb?: PhotoSize;
+	mime_type?: string;
+	file_size?: number;
 }
 
 export interface Voice {
-	readonly file_id: string;
-	readonly duration: number;
-	readonly mime_type?: string;
-	readonly file_size?: number;
+	file_id: string;
+	duration: number;
+	mime_type?: string;
+	file_size?: number;
 }
 
 export interface VideoNote {
-	readonly file_id: string;
-	readonly length: number;
-	readonly duration: number;
-	readonly thumb?: PhotoSize;
-	readonly file_size?: number;
+	file_id: string;
+	length: number;
+	duration: number;
+	thumb?: PhotoSize;
+	file_size?: number;
 }
 
 export interface Contact {
-	readonly phone_number: string;
-	readonly first_name: string;
-	readonly last_name?: string;
-	readonly user_id?: number;
+	phone_number: string;
+	first_name: string;
+	last_name?: string;
+	user_id?: number;
 }
 
 export interface Location {
-	readonly longitude: number;
-	readonly latitude: number;
+	longitude: number;
+	latitude: number;
 }
 
 export interface Venue {
-	readonly location: Location;
-	readonly title: string;
-	readonly address: string;
-	readonly foursquare_id?: string;
+	location: Location;
+	title: string;
+	address: string;
+	foursquare_id?: string;
 }
 
 export interface UserProfilePhotos {
-	readonly total_count: number;
-	readonly photos: PhotoSize[];
+	total_count: number;
+	photos: PhotoSize[];
 }
 
 export interface File {
-	readonly file_id: string;
-	readonly file_size?: number;
-	readonly file_path?: string;
+	file_id: string;
+	file_size?: number;
+	file_path?: string;
 }
 
 export interface ReplyKeyboardMarkup {
-	readonly keyboard: KeyboardButton[][];
-	readonly resize_keyboard?: boolean;
-	readonly one_time_keyboard?: boolean;
-	readonly selective?: boolean;
+	keyboard: KeyboardButton[][];
+	resize_keyboard?: boolean;
+	one_time_keyboard?: boolean;
+	selective?: boolean;
 }
 
 export interface KeyboardButton {
-	readonly text: string;
-	readonly request_contact?: boolean;
-	readonly request_location?: boolean;
+	text: string;
+	request_contact?: boolean;
+	request_location?: boolean;
 }
 
 export interface ReplyKeyboardRemove {
-	readonly remove_keyboard: true;
-	readonly selective?: boolean;
+	remove_keyboard: true;
+	selective?: boolean;
 }
 
 export interface InlineKeyboardMarkup {
-	readonly inline_keyboard: InlineKeyboardButton[][];
+	inline_keyboard: InlineKeyboardButton[][];
 }
 
 export interface InlineKeyboardButton {
-	readonly text: string;
-	readonly url?: string;
-	readonly callback_data?: string;
-	readonly switch_inline_query?: string;
-	readonly switch_inline_query_current_chat?: string;
-	readonly callback_game?: CallbackGame;
-	readonly pay?: boolean;
+	text: string;
+	url?: string;
+	callback_data?: string;
+	switch_inline_query?: string;
+	switch_inline_query_current_chat?: string;
+	callback_game?: CallbackGame;
+	pay?: boolean;
 }
 
 export interface CallbackQuery {
-	readonly id: string;
-	readonly from: User;
-	readonly message?: Message;
-	readonly inline_message_id?: string;
-	readonly chat_instance: string;
-	readonly data?: string;
-	readonly game_short_name?: string;
+	id: string;
+	from: User;
+	message?: Message;
+	inline_message_id?: string;
+	chat_instance: string;
+	data?: string;
+	game_short_name?: string;
 }
 
 export interface ForceReply {
-	readonly force_reply: true;
-	readonly selective?: boolean;
+	force_reply: true;
+	selective?: boolean;
 }
 
 export interface ChatPhoto {
-	readonly small_file_id: string;
-	readonly big_file_id: string;
+	small_file_id: string;
+	big_file_id: string;
 }
 
 export interface ChatMember {
-	readonly user: User;
-	readonly status: string;
-	readonly until_date?: number;
-	readonly can_be_edited?: boolean;
-	readonly can_change_info?: boolean;
-	readonly can_post_messages?: boolean;
-	readonly can_edit_messages?: boolean;
-	readonly can_delete_messages?: boolean;
-	readonly can_invite_users?: boolean;
-	readonly can_restrict_members?: boolean;
-	readonly can_pin_messages?: boolean;
-	readonly can_promote_members?: boolean;
-	readonly can_send_messages?: boolean;
-	readonly can_send_media_messages?: boolean;
-	readonly can_send_other_messages?: boolean;
-	readonly can_add_web_page_previews?: boolean;
+	user: User;
+	status: string;
+	until_date?: number;
+	can_be_edited?: boolean;
+	can_change_info?: boolean;
+	can_post_messages?: boolean;
+	can_edit_messages?: boolean;
+	can_delete_messages?: boolean;
+	can_invite_users?: boolean;
+	can_restrict_members?: boolean;
+	can_pin_messages?: boolean;
+	can_promote_members?: boolean;
+	can_send_messages?: boolean;
+	can_send_media_messages?: boolean;
+	can_send_other_messages?: boolean;
+	can_add_web_page_previews?: boolean;
 }
 
 export interface ResponseParameters {
-	readonly migrate_to_chat_id?: number;
-	readonly retry_after?: number;
+	migrate_to_chat_id?: number;
+	retry_after?: number;
 }
 
 export interface InlineQuery {
-	readonly id: string;
-	readonly from: User;
-	readonly location?: Location;
-	readonly query: string;
-	readonly offset: string;
+	id: string;
+	from: User;
+	location?: Location;
+	query: string;
+	offset: string;
 }
 
 export interface InlineQueryResult {}
